d8403cf2a1cd74ba529b723908eb8b4651566c44
BrunoWagler <reidx0@Outlook.com>
2025-04-03 08:12:00 -0400

    Pagina de inicio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,21 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tela de Bem-vindo</title>
-  @vite('resources/css/app.css')
-</head>
-<body class="bg-blue-200 h-screen flex items-center justify-center">
+@extends('layouts.estrutura')
 
-<div class="text-center p-6 bg-white rounded-lg shadow-lg max-w-lg w-full">
-    <h1 class="text-4xl font-semibold text-gray-700 mb-4">Bem-vindo!</h1>
-    <p class="text-lg text-gray-500 mb-6">Estamos felizes em tê-lo conosco.</p>
+@section('content')
+
+<div class="max-w-lg w-full mx-auto p-6 bg-white rounded-lg shadow-lg">
+    <div class="text-center">
+        <h1 class="text-4xl font-semibold text-gray-700 mb-4">Bem-vindo!</h1>
+    </div>
     
-  <div class="flex gap-2 w-full justify-center">
-    <a href="" class="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-300 mb-4">
-      Mostrar Alunos
-    </a>
-  
-    <a href="{{route('turmas.turmas')}}" class="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-300 mb-4">
-      Mostrar Classes
-    </a>
-  </div>
+    <div class="flex flex-wrap gap-4 justify-center">
+        <a href="{{ route('alunos.estudantes') }}" class="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300 w-full sm:w-auto text-center mb-4">
+            Mostrar Alunos
+        </a>
+        
+        <a href="{{ route('turmas.turmas') }}" class="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300 w-full sm:w-auto text-center mb-4">
+            Mostrar Classes
+        </a>
+    </div>
 </div>
 
-
-</body>
-</html>
+@endsection
